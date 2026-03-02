652a35eb1e8b5119648b4073318f0bdff603a444
drsnow13-tech <drsnow13@gmail.com>
2026-03-02 16:33:55 -0600

    Update Editor.tsx

--- stagesmart.ai/src/pages/Editor.tsx
+++ stagesmart.ai/src/pages/Editor.tsx
@@ -299,10 +299,6 @@ export function Editor() {
   };
 
   const handleTryAnother = () => {
-    setStep('options'); setResults([]); setSelectedOptions(new Set()); setError(null); setActiveResult(null);
-  };
-
-  const handleTryAnother = () => {
     setStep('options'); setResults([]); setSelectedOptions(new Set());
     setError(null); setActiveResult(null);
   };
