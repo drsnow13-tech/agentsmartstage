73f4f03f04c577011e63bc51f81948c6bd633542
drsnow13-tech <drsnow13@gmail.com>
2026-03-11 17:53:03 -0500

    Rename handler function to Editor

--- stagesmart.ai/src/pages/Editor.tsx
+++ stagesmart.ai/src/pages/Editor.tsx
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { neon } from '@neondatabase/serverless';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export const Editor = async (req: VercelRequest, res: VercelResponse) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
